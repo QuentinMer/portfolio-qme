b5fde72852f61dd2186571b03d75188fb8f6ed51
Quentin Mercier <153526325+QuentinMer@users.noreply.github.com>
2025-09-07 17:05:58 +0200

    ajout skills

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -2,47 +2,48 @@ import { useI18n } from '../../i18n/useI18n'
 
 const Skills = () => {
     const { t } = useI18n()
-  return (
-    <section
-    id="skills"
-    className="flex xl:flex-row flex-col xl:gap-64 gap-12 scroll-mt-24 md:scroll-mt-44 xl:scroll-mt-72 my-24 flex items-center justify-start md:mx-12"
->
+    return (
+        <section
+            id="skills"
+            className="flex xl:flex-row flex-col xl:gap-64 gap-12 scroll-mt-24 md:scroll-mt-44 xl:scroll-mt-72 my-24 flex items-center justify-start md:mx-12"
+        >
 
 
-    <div>
-        <h4 className="text-6xl xl:text-8xl font-family-decifer text-orange-500">{t("skills.title")}</h4>
-    </div>
-    <div className="flex flex-row items-center justify-between xl:gap-64 gap-6">
+            <div>
+                <h4 className="text-6xl xl:text-8xl font-family-decifer text-orange-500">{t("skills.title")}</h4>
+            </div>
+            <div className="flex flex-row items-center justify-between xl:gap-64 gap-0">
 
-      
-            <div className="flex flex-col items-center justify-center my-24 cursor-pointer">
-                <a href="https://www.linkedin.com/in/quentin-mercier-77835b297/" target="_blank">
+                <div className="flex flex-col items-center justify-center my-24">
+                    <h4 className="text-xl md:text-2xl font-family-decifer text-slate-50 text-center tracking-widest mb-4">Frontend</h4>
+                    <div className="grid grid-cols-3 gap-x-8 gap-y-2 text-md font-family-geist text-slate-50 text-center">
+                        <div>React</div>
+                        <div>Javascript</div>
+                        <div>Typescript</div>
+                        <div>HTML</div>
+                        <div>CSS</div>
+                        <div>Tailwind</div>
+                        <div>Bootstrap</div>
+                        <div>Material UI</div>
+                        <div>Next.js</div>
+                        <div>Vite</div>
+                    </div>
+                </div>
 
-                <h4 className="text-xl md:text-2xl font-family-decifer text-slate-50 text-center tracking-widest">Linkedin</h4>
-                <img src="/img/linkedinWhite.svg" alt="linkedin" className="md:w-35 md:h-35 w-20 h-20 mt-5 hover:scale-105 transition-transform duration-200" />
-                </a>
+                <div className="flex flex-col items-center justify-center my-24">
+                    <h4 className="text-xl md:text-2xl font-family-decifer text-slate-50 text-center tracking-widest mb-4">Design UI/UX</h4>
+                    <ul className='text-md font-family-geist text-slate-50 text-center space-y-2'>
+                        <li>Figma</li>
+                        <li>figjam</li>
+                        <li>afinity designer</li>
+                        <li>affinity publisher</li>
+                    </ul>
+                </div>
+
             </div>
 
-            <div className="flex flex-col items-center justify-center my-24 cursor-pointer">
-                <a href="https://github.com/QuentinMer" target="_blank">
-
-                <h4 className="text-xl md:text-2xl font-family-decifer text-slate-50 text-center mb-5 tracking-widest">Github</h4>
-                <img src="/img/githubWhite.svg" alt="mail" className="md:w-35 md:h-35 w-20 h-20 hover:scale-105 transition-transform duration-200" />
-                </a>
-            </div>
-            <div className="flex flex-col items-center justify-center my-24 cursor-pointer">
-                <a href="mailto:quentinmer@quentinmercier.dev" target="_blank">
-
-                <h4 className="text-xl md:text-2xl font-family-decifer text-slate-50 text-center mb-5 tracking-widest pb-1">Mail</h4>
-                <img src="/img/mailWhite.svg" alt="mail" className="md:w-35 md:h-35 w-20 h-20 hover:scale-105 transition-transform duration-200" />
-                </a>
-            </div>
-
-
-    </div>
-
-</section>
-  )
+        </section>
+    )
 }
 
 export default Skills
